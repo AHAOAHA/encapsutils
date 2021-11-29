b3f65d21490454a66134796694e4fd229a64ba81
AHAOAHA <ahaoaha_@outlook.com>
2021-11-29 18:59:41 +0800

    fix unitest

--- net_test.go
+++ net_test.go
@@ -8,8 +8,8 @@ import (
 )
 
 func TestPingIPv4(t *testing.T) {
-	convey.Convey("ping www.baidu.com right address", t, func() {
-		convey.So(PingIPV4("www.baidu.com", time.Millisecond*10), convey.ShouldBeTrue)
+	convey.Convey("ping 127.0.0.1 right address", t, func() {
+		convey.So(PingIPV4("127.0.0.1", time.Millisecond*10), convey.ShouldBeTrue)
 	})
 
 	convey.Convey("ping 1.2.3.4 error adress", t, func() {
